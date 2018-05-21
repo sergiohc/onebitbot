b6e655e82462948e837397dd2fcdede0fd29d727
sergiohc <sergioh.c@live.com>
2018-05-21 03:56:26 -0300

    deploy

--- config/database.rb
+++ config/database.rb
@@ -1,34 +1,32 @@
 configure :test do
+  set :database, {
+    adapter: 'postgresql',
+    encoding: 'utf8',
+    database: 'onebitbot_test',
+    pool: 5,
+    username: 'postgres',
+    host: 'postgres'
+  }
+end
+
+configure :development do
    set :database, {
      adapter: 'postgresql',
      encoding: 'utf8',
-     database: 'onebitbot_test',
+     database: 'onebitbot_development',
      pool: 5,
      username: 'postgres',
      host: 'postgres'
    }
 end
 
-configure :development do
- set :database, {
-   adapter: 'postgresql',
-   encoding: 'utf8',
-   database: 'onebitbot_development',
-   pool: 5,
-   username: 'postgres',
-   host: 'postgres'
- }
-end
-
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
-
   set :database, {
-    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    host:     db.host,
-    username: db.user,
-    password: db.password,
-    database: db.path[1..-1],
-    encoding: 'utf8'
+    adapter: 'postgresql',
+    encoding: 'utf8',
+    database: 'onebitbot_production',
+    pool: 5,
+    username: 'postgres',
+    host: 'postgres'
   }
 end
